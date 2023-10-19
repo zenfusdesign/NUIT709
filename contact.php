7076340159570675d02193392419fc78493907c5
Marius Duponcel <marioduponcel@gmail.com>
2023-10-19 13:00:46 +0200

    sendgrid

--- contact.php
+++ contact.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'vendor/autoload.php';
+
 if(!$_POST) exit;
 
 // Email address verification, do not edit.
@@ -32,55 +34,41 @@ if(trim($name) == '') {
 } else if(trim($verify) != '4') {
 	echo '<div class="error_message">Attention! The verification number you entered is incorrect.</div>';
 	exit();
-} 
+}
 
 $comments = htmlspecialchars($comments);
 
-
-// Configuration option.
-// Enter the email address that you want to emails to be sent to.
-
-//$address = "example@youremail.net";
 $address = "mariusduponcel@gmail.com";
-
-
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
-
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
-
 $e_subject = 'You\'ve been contacted by ' . $name . '.';
-
-
-// Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
-
 $e_body = "You have been contacted by $name, their additional message is as follows." . PHP_EOL . PHP_EOL;
 $e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
 $e_reply = "You can contact $name via email, $email";
-
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+$msg = wordwrap($e_body . $e_content . $e_reply, 70);
 
 $headers = "From: $email" . PHP_EOL;
 $headers .= "Reply-To: $email" . PHP_EOL;
-$headers .= "MIME-Version: 1.0" . PHP_EOL;
-$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+$headers .$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 
-if(mail($address, $e_subject, $msg, $headers)) {
+$emailToSend = new \SendGrid\Mail\Mail();
+$emailToSend->setFrom($email, $name);
+$emailToSend->setSubject($e_subject);
+$emailToSend->addTo($address);
+$emailToSend->addContent("text/plain", $msg);
 
-	// Email has sent successfully, echo a success page.
-
-	echo "<fieldset>";
-	echo "<div id='success_page'>";
-	echo "<h3>Email Sent Successfully.</h3>";
-	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
-	echo "</div>";
-	echo "</fieldset>";
-
-} else {
-
-	echo 'ERROR!';
-
+try {
+    $response = $sendgrid->send($emailToSend);
+    if ($response->statusCode() == 202) {
+        echo "<fieldset>";
+        echo "<div id='success_page'>";
+        echo "<h3>Email Sent Successfully.</h3>";
+        echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
+        echo "</div>";
+        echo "</fieldset>";
+    } else {
+        echo 'ERROR!';
+    }
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
 }
+
+?>
